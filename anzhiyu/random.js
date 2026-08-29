34049e815978f340cafd3028995f0e39faa96242
76hvc <b124308016@stu.ahu.edu.cn>
2026-08-29 14:19:14 +0800

    Site updated: 2026-08-29 14:19:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/29/1/"];function toRandomPost(){
+var posts=["2026/08/29/免费域名托管到cloudflare/","2026/08/29/！！！2026最新免费高速vpn部署教程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
